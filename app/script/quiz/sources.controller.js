fab6c2149bd1ea1b887a1b7e490b0959ca09dcb9
Oksana <klimovaoxana.v@gmail.com>
2016-10-23 23:25:18 +0300

    Add: angular for sources form

--- app/script/quiz/sources.controller.js
+++ app/script/quiz/sources.controller.js
@@ -4,9 +4,50 @@ angular.module('app')
 
     .controller('SourcesCtrl', SourcesController);
 
-function SourcesController(ngDialog) {
+function SourcesController($scope, ngDialog) {
 
     var self = this;
+
+    $scope.ages = ['18-', '18+'];
+    $scope.ageRes = {val: ''};
+
+    $scope.socials = ['1', '2', '3', '4', '5'];
+    $scope.socialRes = {val: ''};
+
+    $scope.cognitives = ['1', '2', '3', '4', '5'];
+    $scope.cognitiveRes = {val: ''};
+
+    $scope.emotionals = ['1', '2', '3', '4', '5'];
+    $scope.emotionalRes = {val: ''};
+
+    $scope.images = ['1', '2', '3', '4', '5'];
+    $scope.imageRes = {val: ''};
+
+    $scope.families = ['1', '2', '3', '4', '5'];
+    $scope.familyRes = {val: ''};
+
+    $scope.works = ['1', '2', '3', '4', '5'];
+    $scope.workRes = {val: ''};
+
+    $scope.nonstandards = ['1', '2', '3', '4', '5'];
+    $scope.nonstandardRes = {val: ''};
+
+    $scope.healths = ['1', '2', '3', '4', '5'];
+    $scope.healthRes = {val: ''};
+
+    $scope.financials = ['1', '2', '3', '4', '5'];
+    $scope.financialdRes = {val: ''};
+
+    $scope.locations = ['1', '2', '3', '4', '5'];
+    $scope.locationdRes = {val: ''};
+
+    $scope.religions = ['1', '2', '3', '4', '5'];
+    $scope.religionsdRes = {val: ''};
+
+    $scope.homophobias = ['1', '2', '3', '4', '5'];
+    $scope.homophobiadRes = {val: ''};
+
+
 
     // self.clickToOpen = function() {
     //     ngDialog.open({
