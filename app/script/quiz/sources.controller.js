aafd88d60ddd368f60ad4b0ea9cd0a9cefaa08b9
Oksana <klimovaoxana.v@gmail.com>
2016-10-24 00:49:29 +0300

    Add: count for resource page

--- app/script/quiz/sources.controller.js
+++ app/script/quiz/sources.controller.js
@@ -6,7 +6,8 @@ angular.module('app')
 
 function SourcesController($scope, ngDialog) {
 
-    var self = this;
+    var self = this,
+        age, social, cognitive, emotional, image, family, work, nonstandard, health, financial, location, religion, homophobia, isolation, openness, common;
 
     $scope.ages = ['18-', '18+'];
     $scope.ageRes = {val: ''};
@@ -36,16 +37,62 @@ function SourcesController($scope, ngDialog) {
     $scope.healthRes = {val: ''};
 
     $scope.financials = ['1', '2', '3', '4', '5'];
-    $scope.financialdRes = {val: ''};
+    $scope.financialRes = {val: ''};
 
     $scope.locations = ['1', '2', '3', '4', '5'];
-    $scope.locationdRes = {val: ''};
+    $scope.locationRes = {val: ''};
 
     $scope.religions = ['1', '2', '3', '4', '5'];
-    $scope.religionsdRes = {val: ''};
+    $scope.religionRes = {val: ''};
 
     $scope.homophobias = ['1', '2', '3', '4', '5'];
-    $scope.homophobiadRes = {val: ''};
+    $scope.homophobiaRes = {val: ''};
+
+
+    $scope.checkAge = function () {
+        age = $scope.ageRes.val;
+
+        return age;
+
+    };
+
+    $scope.checkIsolation = function () {
+        social = $scope.socialRes.val,
+        cognitive = $scope.cognitiveRes.val,
+        emotional = $scope.emotionalRes.val;
+
+        isolation = +social + +cognitive + +emotional;
+
+        return isolation;
+    };
+
+
+    $scope.checkOpenness = function () {
+        image = $scope.imageRes.val,
+        family = $scope.familyRes.val,
+        work = $scope.workRes.val;
+
+        openness = +image + +family + +work;
+
+        return openness;
+    };
+
+
+    $scope.checkCommon = function () {
+        nonstandard = $scope.nonstandardRes.val,
+        health = $scope.healthRes.val,
+        financial = $scope.financialRes.val,
+        location = $scope.locationRes.val,
+        religion = $scope.religionRes.val,
+        homophobia = $scope.homophobiaRes.val;
+
+        common = +nonstandard + +health + +financial + +location + +religion + +homophobia;
+
+        return common;
+    };
+
+
+
 
 
 
