69f49ac4b2cb39662624fd4d7804bc868e879972
Oksana <klimovaoxana.v@gmail.com>
2017-04-03 10:13:25 +0300

    Add popup content for ChooseCharacter

--- Components/Quiz/ChooseCharacter/ChooseCharacter.component.js
+++ Components/Quiz/ChooseCharacter/ChooseCharacter.component.js
@@ -34,12 +34,46 @@
             return currentCharacter;
         };
 
-        // $ctrl.clickToOpen = function() {
-        //     ngDialog.open({
-        //         template: '../templates/main-modal.html',
-        //         appendTo: '.main-screen'
-        //     });
-        // };
+
+        $ctrl.clickToOpen = function() {
+            ngDialog.open({
+                plain: true,
+                controller: 'Popup',
+                template: '<div class="popup-content"><p>Выберите точку зрения из четырех возможных. Кликните на знак вопроса рядом с соответствующим названием, если необходимо уточнение, а затем на фигурку персонажа. Далее нажмите на стрелку в нижнем правом углу страницы. Если ни одна из точек зрения все равно не подходит, можно выбрать любую или вернуться на стартовую страницу к Памяткам и Контактам.</p></div>'
+            });
+        };
+
+        $ctrl.clickToOpenGirl = function() {
+            ngDialog.open({
+                plain: true,
+                controller: 'Popup',
+                template: '<div class="popup-content"><p>Если ты обнаружила в себе влечение к девушкам и/или ведешь себя гендерно неконформно, а также если ты позиционируешь себя как ЛБТИК.</p></div>'
+            });
+        };
+
+        $ctrl.clickToOpenFriend = function() {
+            ngDialog.open({
+                plain: true,
+                controller: 'Popup',
+                template: '<div class="popup-content"><p>Если твоя подруга обнаружила в себе влечение к девушкам и/или ведет себя гендерно неконформно, а также если твоя подруга позиционирует себя как ЛБТИК.</p></div>'
+            });
+        };
+
+        $ctrl.clickToOpenParent = function() {
+            ngDialog.open({
+                plain: true,
+                controller: 'Popup',
+                template: '<div class="popup-content"><p>Если Ваша дочь (младшая родственница) обнаружила в себе влечение к девушкам и/или ведет себя гендерно неконформно, а также если она позиционирует себя как ЛБТИК.</p></div>'
+            });
+        };
+
+        $ctrl.clickToOpenHelper = function() {
+            ngDialog.open({
+                plain: true,
+                controller: 'Popup',
+                template: '<div class="popup-content"><p>Если Вы педагог, врач, социальный работник или равный консультант в каком-либо сообществе, а также если Вы психолог, не знакомый или мало знакомый с ЛГБТИК-проблематикой. И если к Вам обратилась девушка, обнаружившая в себе влечение к девушкам и/или ведущая себя гендерно неконформно, либо позиционирующая себя как ЛБТИК, а также если к Вам обратился кто-то из ее друзей или родных.</p></div>'
+            });
+        };
 
     }
 })();
